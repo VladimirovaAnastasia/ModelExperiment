81a4e86ccd06ff1ad6fa6dd46d54f2a59e1ddd7c
KRASAVA <vladimirova.a-a@yandex.ru>
2019-12-08 22:53:02 +0300

    add results of training

--- experiments_model/models/network.py
+++ experiments_model/models/network.py
@@ -2,7 +2,6 @@ import numpy as np
 import math
 import numpy.random as r
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
 
 
 def f(x):
@@ -56,6 +55,23 @@ def calculation_hidden_layer_delta(delta_layer_delta, w_l, z_l):
     return np.dot(np.transpose(w_l), delta_layer_delta) * derivative_f(z_l)
 
 
+def predict_y(Weights, betta, X, Y):
+    error_func = []
+    m = len(Y)
+    for j in range(m):
+        h, z = calculation_of_sum_and_F_activation(X[j, :], Weights, betta)
+        print(j)
+        print(h[3])
+        print(Y[j, 0])
+        error = pow((Y[j, 0] - h[3]), 2)
+        error_func.append(error)
+
+    plt.plot(error_func)
+    plt.ylabel('Погрешность обучения')
+    plt.xlabel('Выборки')
+    plt.show()
+
+
 def getPrediction(x, y, delay):
     counter = 0
     alpha = 0.1
@@ -72,13 +88,25 @@ def getPrediction(x, y, delay):
     epoch = 500
     structure = [enter, hidden_layout, exit]
 
-
     coef = (np.sqrt(np.sum(np.square(x), axis=1)))
     X_new = np.repeat(coef, enter).reshape(len(x), enter)
     X = x / X_new
     Y = y
-    X_train = np.vstack((X[0:len(x), 0:enter]))
-    Y_train = np.vstack((Y[0:len(x), 0]))
+    print(len(x))
+    trainData = len(x) * 9 // 10
+    print(trainData)
+    testData = len(x) - trainData
+    print(testData)
+    X_train = np.vstack((X[0:trainData, 0:enter]))
+    Y_train = np.vstack((Y[0:trainData, 0]))
+    print(trainData)
+    print(testData)
+    X_test = np.vstack((X[trainData:len(x), 0:enter]))
+    print(X_test)
+    Y_test = np.vstack((Y[trainData:len(x), 0]))
+
+    # X_train = np.vstack((X[0:len(x), 0:enter]))
+    # Y_train = np.vstack((Y[0:len(x), 0]))
 
     W, b = setup_and_init_weights(structure)
 
@@ -91,7 +119,6 @@ def getPrediction(x, y, delay):
             h, z = calculation_of_sum_and_F_activation(X_train[i, :], W, b)
             delta[3] = calculation_layer_delta(Y_train[i, 0], h[3], z[3])
             k += pow((Y_train[i, 0] - h[3]), 2)
-            print('погрешность',k)
             delta[2] = calculation_hidden_layer_delta(delta[3], W[2], z[2])
             W_delta[2] += np.dot(delta[3][:, np.newaxis], np.transpose(h[2][:, np.newaxis]))
             b_delta[2] += delta[3]
@@ -114,25 +141,25 @@ def getPrediction(x, y, delay):
         # k = math.sqrt(s / (n * (p - 1))) * X_new
         k = math.sqrt(s / (n * (epoch - 1)))
         if counter == 1:
-            print(k)
+            print(1)
         if counter == 25:
-            print(k)
+            print(25)
         if counter == 50:
-            print(k)
+            print(50)
         if counter == 75:
-            print(k)
+            print(75)
         if counter == 100:
-            print(k)
+            print(100)
         if counter == 125:
-            print(k)
+            print(125)
         if counter == 150:
-            print(counter)
+            print(150)
         if counter == 200:
-            print(counter)
+            print(200)
         if counter == 400:
-            print(k)
+            print(400)
         if counter == 600:
-            print(k)
+            print(600)
         k_func.append(k)
         counter += 1
 
@@ -141,5 +168,4 @@ def getPrediction(x, y, delay):
     plt.xlabel('Количество итераций')
     plt.show()
 
-    return 'ok'
-
+    predict_y(W, b, X_test, Y_test)
